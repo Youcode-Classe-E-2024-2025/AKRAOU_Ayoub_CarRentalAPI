858829f5852264671795580a2331530c0c173f72
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-03-13 10:57:26 +0000

    creer la relation un a plusieurs entre le model User et le model Rental

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
